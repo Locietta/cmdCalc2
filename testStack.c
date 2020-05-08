72999799ddc33248e087a81401a2527f89e602ca
Locietta <2324566152@qq.com>
2020-05-09 00:02:12 +0800

    new a test.c for stack

---
+++ testStack.c
@@ -0,0 +1,27 @@
+#include "stack.h"
+#include <stdio.h>
+
+int main(void) {
+    stack mystack = newStack(int);
+    int input = 0, out = 0;
+    for (int i = 0; i < 5; ++i) {
+        scanf("%d", &input);
+        mystack.push(&mystack, &input);
+    }
+    while (mystack.size - 1) {
+        mystack.pop(&mystack, &out);
+        printf("%d ", out);
+    }
+    putchar('\n');
+    for (int i = 0; i < 3; ++i) {
+        scanf("%d", &input);
+        mystack.push(&mystack, &input);
+    }
+    printf("%d\n", *(int *)mystack.top(&mystack));
+    while (mystack.size) {
+        mystack.pop(&mystack, &out);
+        printf("%d ", out);
+    }
+    mystack.destroy(&mystack);
+    return 0;
+}
