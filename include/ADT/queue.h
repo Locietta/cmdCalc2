94c3bd813bbbb2752c1d8c6792c51b001517ef2f
Locietta <2324566152@qq.com>
2020-05-09 18:39:05 +0800

    comments added

--- include/ADT/queue.h
+++ include/ADT/queue.h
@@ -19,24 +19,107 @@ struct queue_info {  // container
 
     /* Public Function Prototypes */
 
+    /**
+     * Function: empty
+     * Usage: myqueue.empty(&myqueue);
+     * ---------------------------
+     * This function returns 1 if the queue is empty,
+     * otherwise, returns 0;
+     */
+
     int (*empty)(queue *this);
+
+    /**
+     * Function: front
+     * Usage: int n = *(int *) myqueue.front(&myqueue)
+     * ---------------------------
+     * This function return a pointer pointing to the first element
+     * of the queue. It will NOT delete the first element, and you 
+     * can't use this pointer as a lvalue
+     */
 
     const void *(*front)(queue *this);
 
+    /**
+     * Function: back
+     * Usage: int n = *(int *) myqueue.back(&myqueue)
+     * ---------------------------
+     * This function return a pointer pointing to the last element
+     * of the queue. It will NOT delete the last element, and you 
+     * can't use this pointer as a lvalue
+     */
+
     const void *(*back)(queue *this);
+
+    /**
+     * Function: push
+     * Usage: myqueue.push(&myqueue, &input);
+     * ---------------------------
+     * This function COPY the data pointed by a pointer into
+     * a new element in the queue. Later, when variable "input" changes
+     * its value, the element in queue will NOT change.
+     * if everything is OK, it shall return 0, 
+     * otherwise, it returns a non-zero value indicating an error. 
+     */
 
     int (*push)(queue *this, void *data);
 
+    /**
+     * Function: pop
+     * Usage: myqueue.pop(&myqueue, &buffer);
+     * ---------------------------
+     * This function copy the first element into the datafield pointed
+     * by &buffer. And then DELETE the first element in the queue.
+     * In case of empty queue, it returns a non-zero value (1),
+     * if everything is OK, it shall return 0
+     */
+
     int (*pop)(queue *this, void *target);
 
+    /**
+     * Function: clear
+     * Usage: myqueue.clear(&myqueue);
+     * ---------------------------
+     * This function clear all the data in the queue.
+     * So the queue object can later be reused.
+     * If you wanna dispose this queue, DON NOT use it,
+     * use destory() instead.
+     * (Or you'll get a leakage)
+     */
+
     void (*clear)(queue *this);
+
+    /**
+     * Function: destory
+     * Usage: myqueue.destory(&myqueue);
+     * ---------------------------
+     * This function free all the data in the queue.
+     * You have to reinitialize it with newQueue() if you
+     * wanna use it again.
+     * When you want to dispose a queue, use destory() instead of clear()
+     */
 
     void (*destory)(queue *this);
 };
 
 /* Construction Function */
 
+/**
+ * Function: queueInit
+ * Usage: you should use newQueue() instead.
+ * ---------------------------
+ * This function initialize a queue.
+ */
+
 queue queueInit(size_t size);
+
+/**
+ * Function: newQueue
+ * Usage: myqueue = newQueue(double);
+ * ---------------------------
+ * This function initialize a new queue with given type.
+ * In case of failure to build a queue, it returns a NULL_QUEUE
+ */
 
 #define newQueue(type) queueInit(sizeof(type))
 
