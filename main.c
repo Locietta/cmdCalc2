831690d0ed38fa9be0cef8b4f298578bd34c088f
Locietta <2324566152@qq.com>
2020-05-10 12:43:28 +0800

    framework done

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include "stack.h"
+#include "queue.h"
+#include "color.h"
+#include "calcio.h"
+#include "calc.h"
 #include <stdio.h>
 
+void titleLog(void);
+
 int main(void) {
-    stack mystack = newStack(int);
-    int input = 0, out = 0;
-    for (int i = 0; i < 5; ++i) {
-        scanf("%d", &input);
-        mystack.push(&mystack, &input);
+    char expr[100];
+    double result = 0;
+    setColor(WHITE);
+    titleLog();
+    colorPrintf(PURPLE, "Input > ");
+    while (Expr_gets(expr, 100)) {
+        if (*expr && !Calc(expr, &result)) {
+            colorPrintf(PINK, "result: ");
+            colorPrintf(LIGHTCYAN, "%g\n", result);
+        }
+        colorPrintf(PURPLE, "Input > ");
     }
-    while (mystack.size - 1) {
-        mystack.pop(&mystack, &out);
-        printf("%d ", out);
-    }
-    putchar('\n');
-    for (int i = 0; i < 3; ++i) {
-        scanf("%d", &input);
-        mystack.push(&mystack, &input);
-    }
-    printf("%d\n", *(int *)mystack.top(&mystack));
-    while (mystack.size) {
-        mystack.pop(&mystack, &out);
-        printf("%d ", out);
-    }
-    mystack.destroy(&mystack);
+    colorPrintf(CYAN, "See you ~");
+    Sleep(1000); // wait for a second ( 
     return 0;
+}
+
+void titleLog(void) {
+    printf("This is a ");
+    colorPrintf(BLUE, "command line calculator");
+    printf(".\nJust type expresions freely, and we'll calculate it.\nAnd... if you wanna a ");
+    colorPrintf(YELLOW, "quit, ");
+    printf("type in ");
+    colorPrintf(LIGHTRED, "\'^z\' ");
+    printf("(");
+    colorPrintf(LIGHTRED, "ctrl ");colorPrintf(YELLOW, "+ ");colorPrintf(LIGHTRED, "z");
+    printf(") and the program would be ended.\n");
 }
